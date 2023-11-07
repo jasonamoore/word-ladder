23606d6d61598c1ee6bb042ea27ca7421bc856a1
Jason Moore <mooreja10@appstate.edu>
2023-11-06 23:45:21 -0500

    Added dictionary and basic game logic for input checking. Slight (bad) design updates.

--- app/src/main/java/com/example/wordladder/WordLadderFragment.kt
+++ app/src/main/java/com/example/wordladder/WordLadderFragment.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -20,6 +20,13 @@ class WordLadderFragment : Fragment() {
     private val binding : FragmentWordLadderBinding
         get() = checkNotNull(_binding) {
         "Binding is null!"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // load up stuff
+        viewModel.loadWordTable(
+            resources.openRawResource(R.raw.dictionary))
     }
 
     override fun onCreateView(
@@ -38,20 +45,52 @@ class WordLadderFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupUI()
+    }
 
-        // test code
-        binding.submitWord.setOnClickListener {
-            val inputText = binding.editText.getText().toString()
-            val result = viewModel.wordSubmitted(inputText)
-            if (result)
-                Snackbar.make(binding.root, R.string.success,
-                    Snackbar.LENGTH_SHORT).show()
-        }
-
-        binding.prevWord.setOnClickListener {
-            val prev = viewModel.getPreviousWord()
-            prev?.let {
-                binding.editText.setText(it, TextView.BufferType.EDITABLE)
+    private fun setupUI() {
+        binding.apply {
+            // on text change we should force uppercase
+            editText.doOnTextChanged { etext, _, _, _ ->
+                val text = etext.toString()
+                // break string on cursor position:
+                val cursorPos = editText.selectionStart
+                val sanitizeLeft = sanitize(text.substring(0, cursorPos))
+                val sanitizeAll = sanitize(text)
+                if (text != sanitizeAll) {
+                    editText.setText(sanitizeAll)
+                    // smart way to move cursor (length of sanitized left split)
+                    editText.setSelection(sanitizeLeft.length)
+                }
+            }
+            // submit a word to the view model
+            submitWord.setOnClickListener {
+                val inputText = editText.text.toString().uppercase()
+                if (!viewModel.validLadderWord(inputText)) {
+                    Snackbar.make(
+                        root, "Bad input!",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+                val result = viewModel.wordSubmitted(inputText)
+                if (result) {
+                    Snackbar.make(
+                        root, R.string.success,
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                } else {
+                    updateRecycler(viewModel.historyList)
+                    editText.setText("") // clear input
+                }
+            }
+            // get previous word from view model
+            prevWord.setOnClickListener {
+                val prev = viewModel.getPreviousWord()
+                prev?.let {
+                    editText.setText(it, TextView.BufferType.EDITABLE)
+                    updateRecycler(viewModel.historyList)
+                }
             }
         }
     }
@@ -61,8 +100,18 @@ class WordLadderFragment : Fragment() {
         _binding = null
     }
 
-    fun updateRecycler(list: List<String>) {
+    private fun updateRecycler(list: List<String>) {
         binding.recyclerView.adapter = LadderAdapter(list)
+    }
+
+    private fun sanitize(text: String): String {
+        val sanitized = StringBuilder()
+        text.forEach {
+            val u = it.uppercaseChar()
+            if (u in 'A'..'Z')
+                sanitized.append(u)
+        }
+        return sanitized.toString()
     }
 
 }
--- app/src/main/java/com/example/wordladder/WordLadderViewModel.kt
+++ app/src/main/java/com/example/wordladder/WordLadderViewModel.kt
@@ -1,49 +1,78 @@
 package com.example.wordladder
 
 import androidx.lifecycle.ViewModel
+import java.io.File
+import java.io.InputStream
 import java.util.Stack
+import kotlin.math.abs
+
+const val WORD_SIZE = 4
 
 class WordLadderViewModel : ViewModel() {
 
-    val target: String = "test"
+    private val target: String = "test"
 
     val historyList: List<String>
-        get() = wordStack.toList()
+        get() = wordStack.toList().reversed()
 
-    private val wordStack: Stack<String> = loadHistoryStack()
-    private var currentWord: String = if (!wordStack.isEmpty()) wordStack.peek() else ""
-        get() = currentWord
+    private lateinit var wordTable: HashSet<String>
+    private var wordStack: Stack<String> = Stack()
+    //private lateinit var wordStack: Stack<String>
+    //private lateinit var currentWord: String
+    private var currentWord: String = ""
 
-
-    private fun loadHistoryStack(): Stack<String> {
-        // this will retrieve stored data from user prefs
-        return Stack<String>()
+    // default word
+    init {
+        wordStack.push("WORD")
     }
 
-    fun getPreviousWord(): String? {
-        return if (!wordStack.isEmpty()) {
+    fun getPreviousWord(): String {
+        return if (wordStack.size > 1) {
             currentWord = wordStack.pop()
             currentWord
-        } else null
+        } else wordStack.peek()
+    }
+
+    fun peekPreviousWord(): String {
+        return wordStack.peek()
     }
 
     fun wordSubmitted(word: String): Boolean {
-        pushWord(currentWord)
+        wordStack.push(word)
         currentWord = word
         return checkWord(currentWord)
-    }
-
-    private fun pushWord(word: String) {
-        wordStack.push(word)
     }
 
     private fun checkWord(word: String): Boolean {
         return word == target
     }
 
-    private fun updateRecycler() {
-        // this should pass the new stack data to the recycler
-        // and call updateUI on fragment
+    fun validLadderWord(word: String): Boolean {
+        val prev = peekPreviousWord()
+        // must be different from prev
+        if (word == prev) return false
+        // false if not a real word
+        if (!wordTable.contains(word)) return false
+        var wordDiff = 0
+        for (i in 0 until WORD_SIZE)
+            wordDiff += if (word[i] != prev[i]) 1 else 0
+        // true if only different by 1 char
+        if (wordDiff == 1)
+            return true
+        // if it is a rearrangement
+        if (word.toSet() == prev.toSet())
+            return true
+        return false
+    }
+
+    fun loadHistoryStack(): Stack<String> {
+        // this will retrieve stored data from user prefs
+        return Stack<String>()
+    }
+
+    fun loadWordTable(dictStream: InputStream) {
+        wordTable = HashSet()
+        dictStream.bufferedReader().forEachLine { wordTable.add(it.uppercase()) }
     }
 
 }
